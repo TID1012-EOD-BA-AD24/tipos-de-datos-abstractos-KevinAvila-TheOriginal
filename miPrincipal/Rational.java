7c1c52b2f6f02c9ba489cbc1a617dd0ff480622e
Kevin Alejandro Avila Medina <l23170038@culiacan.tecnm.mx>
2024-12-09 06:27:01 +0000

    08

--- miPrincipal/Rational.java
+++ miPrincipal/Rational.java
@@ -4,10 +4,8 @@ public class Rational{
     private int r[] = new int[2];
 
     public Rational(){
-       //coloca aquí el código faltante
-       
-           this.r[0] = 1;
-           this.r[1] =1;
+      r[0] = 0;
+      r[1] = 1;
     }
 
     public Rational(int a,int b) throws PosicionIlegalException{
@@ -15,7 +13,8 @@ public class Rational{
         {
             throw new PosicionIlegalException();
         }
-       //coloca aquí el código faltante
+        r[0] = a;
+        r[1] = b;
     }
     
     public int getNumerador(){
@@ -38,11 +37,15 @@ public class Rational{
         return x;
     }
     public Rational add(Rational a){
-      //coloca aquí el código faltante
-      Rational x = new Rational();
-    x.setNumerador(a.getNumerador()*b.getNumerador());
-    x.setDenominador(b.getDenominador()*b.getDenominador());
-    return x;
+        Rational x = new Rational();
+        x.setNumerador(this.getNumerador()*a.getDenominador()+this.getDenominador()*a.getNumerador());
+        x.setDenominador(this.getDenominador()*a.getDenominador());
+        return x;
+        /*Rational x = new Rational();
+        x.setNumerador(a.getDenominador() * b.getNumerador() + b.getDenominador()* a.getNumerador());
+        x.setDenominador(a.getDenominador() * b.getDenominador());
+        return x;
+        */
     }
 
     public Rational mult(Rational a, Rational b){
@@ -52,27 +55,23 @@ public class Rational{
         return x;
     }
     public Rational mult(Rational a){
-       //coloca aquí el código faltante
-       Rational x = new Rational();
-    x.setNumerador(a.getNumerador()*b.getNumerador());
-    x.setDenominador(a.getDenominador()*b.getDenominador());
-    return x;
+        Rational x = new Rational();
+       x.setNumerador(this.getNumerador()*a.getNumerador());
+       x.setDenominador(this.getDenominador()*a.getDenominador());
+       return x;
     }
     public boolean equal(Rational a,Rational b){
 
         return a.getDenominador()*b.getNumerador() == b.getDenominador()*a.getNumerador();
     }
     public boolean equal(Rational a){
-        //coloca aquí el código faltante
-        return (a.getNumerador()*b.getDenominador() == 
-        a.getDenominador()*b.getNumerador());
-   
+        return (this.getDenominador()*a.getNumerador() == 
+        a.getDenominador()*this.getNumerador());
     }
     @Override
     public String toString() {
-        //coloca aquí el código faltante
-        
-            return this.r[0]+"/"+this.r[1];
+        return r[0] + "/" +r[1];
+
     }
     
 
